277ee10cd9b727c9a98dd23e083f988192cfacc9
Omshah00 <aricdesai02@gmail.com>
2025-06-03 21:53:08 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,7 +1,6 @@
 from algosdk.v2client import algod
-from algosdk.future.transaction import ApplicationCallTxn, StateSchema, OnComplete
+from algosdk.future.transaction import ApplicationCreateTxn, StateSchema, OnComplete
 from algosdk import account, mnemonic
-from algosdk.logic import get_application_address
 import json
 
 ALGOD_ADDRESS = "https://testnet-api.algonode.cloud"
@@ -16,29 +15,40 @@ def deploy_app(creator_mnemonic):
     private_key = mnemonic.to_private_key(creator_mnemonic)
     sender = account.address_from_private_key(private_key)
 
+    # Compile PyTeal to TEAL
     approval_teal = compileTeal(dice_game.approval_program(), mode=Mode.Application, version=6)
     clear_teal = compileTeal(dice_game.clear_state_program(), mode=Mode.Application, version=6)
 
+    # Compile TEAL to bytes using algod
     approval_result = algod_client.compile(approval_teal)
     clear_result = algod_client.compile(clear_teal)
+    approval_program = bytes.fromhex(approval_result["result"])
+    clear_program = bytes.fromhex(clear_result["result"])
 
+    # Define state schemas
     global_schema = StateSchema(num_uints=1, num_byte_slices=0)
     local_schema = StateSchema(num_uints=0, num_byte_slices=0)
 
+    # Get suggested params
     params = algod_client.suggested_params()
-    txn = ApplicationCallTxn(
-        sender,
-        params,
-        OnComplete.NoOpOC,
-        approval_result["result"],
-        clear_result["result"],
-        global_schema,
-        local_schema
+
+    # Create application creation transaction
+    txn = ApplicationCreateTxn(
+        sender=sender,
+        sp=params,
+        on_complete=OnComplete.NoOpOC,
+        approval_program=approval_program,
+        clear_program=clear_program,
+        global_schema=global_schema,
+        local_schema=local_schema
     )
+
+    # Sign and send the transaction
     signed_txn = txn.sign(private_key)
     txid = algod_client.send_transaction(signed_txn)
     print("Transaction ID:", txid)
 
+    # Wait for confirmation
     from algosdk.future.transaction import wait_for_confirmation
     response = wait_for_confirmation(algod_client, txid, 4)
     print("Deployed App ID:", response["application-index"])
